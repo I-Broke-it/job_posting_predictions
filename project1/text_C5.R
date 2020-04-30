b711a18d1d15b83a54a316018f11181deb0c6145
I-Broke-it <frankpbac246@gmail.com>
2020-04-30 18:45:12 -0500

    add code to perform KSVM and C5 on all text features

---
+++ project1/text_C5.R
@@ -0,0 +1,140 @@
+
+#written by Francisco Baca
+
+#TESTING ALTERNATIVE MODEL C5 ON THE DATA
+rm(list = ls())
+gc()
+
+###INCOMPLETE###
+library(magrittr) #for using pipes (more concise code, but slightly less explicit)
+#text mining libraries
+library(dplyr)
+library(caret)  #for pre-processing
+library(C50)
+#library(kernlab)
+library(data.table) #for memory efficiency
+#library(doParallel)
+
+#TOO BIG FOR C5 AND MY CURRENT HARDWARE, RESULTS IN STACK OVERFLOWS AND SEG FAULTS
+#performing C5 on description attribute
+cat("performing C5 on description attribute\n\n")
+train_dtm <- fread("./text_data/description_train_DTM.csv")
+test_dtm <- fread("./text_data/description_test_DTM.csv")
+mc <- table(test_dtm$fraudulent)[2]
+#finding the column name intersection between training and testing partitions
+#so the terms match, (partitions are still different and valid), just making sure
+#the fields match and anything extra gets discarded 
+intersect_cols <- intersect(colnames(train_dtm), colnames(test_dtm))
+train_dtm <- as.data.table(train_dtm[, ..intersect_cols])
+test_dtm <- as.data.table(test_dtm[, ..intersect_cols])
+gc()
+train_dtm$fraudulent %<>% factor
+test_dtm$fraudulent %<>% factor
+C5 <- C5.0(fraudulent~., data=train_dtm, control = C5.0Control(minCases=mc))
+gc()
+#testing prediction performance on training data with model trained with training data
+cat("testing prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(C5, train_dtm)
+description_train_train_cm <- confusionMatrix(predictions, train_dtm$fraudulent, positive='1')
+print(description_train_train_cm)
+cat("\n\n")
+gc()
+cat("testing prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(C5, test_dtm)
+description_train_test_cm <- confusionMatrix(predictions, test_dtm$fraudulent, positive='1')
+print(description_train_test_cm)
+cat("\n\n\n")
+gc()
+
+
+#performing C5 on requirements attribute
+cat("performing C5 on requirements attribute\n\n")
+train_dtm <- fread("./text_data/requirements_train_DTM.csv")
+test_dtm <- fread("./text_data/requirements_test_DTM.csv")
+mc <- table(test_dtm$fraudulent)[2]
+train_dtm$fraudulent %<>% factor
+test_dtm$fraudulent %<>% factor
+#finding the column name intersection between training and testing partitions
+#so the terms match, (partitions are still different and valid), just making sure
+#the fields match and anything extra gets discarded 
+intersect_cols <- intersect(colnames(train_dtm), colnames(test_dtm))
+train_dtm <- as.data.table(train_dtm[, ..intersect_cols])
+test_dtm <- as.data.table(test_dtm[, ..intersect_cols])
+gc()
+C5 <- C5.0(fraudulent~., data=train_dtm, control = C5.0Control(minCases=mc))
+gc()
+#testing prediction performance on training data with model trained with training data
+cat("testing prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(C5, train_dtm)
+requirements_train_train_cm <- confusionMatrix(predictions, train_dtm$fraudulent, positive='1')
+print(requirements_train_train_cm)
+cat("\n\n")
+gc()
+cat("testing prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(C5, test_dtm)
+requirements_train_test_cm <- confusionMatrix(predictions, test_dtm$fraudulent, positive='1')
+print(requirements_train_test_cm)
+cat("\n\n\n")
+gc()
+
+
+#performing C5 on benefits attribute
+cat("performing C5 on benefits attribute\n\n")
+train_dtm <- fread("./text_data/benefits_train_DTM.csv")
+test_dtm <- fread("./text_data/benefits_test_DTM.csv")
+mc <- table(test_dtm$fraudulent)[2]
+train_dtm$fraudulent %<>% factor
+test_dtm$fraudulent %<>% factor
+#finding the column name intersection between training and testing partitions
+#so the terms match, (partitions are still different and valid), just making sure
+#the fields match and anything extra gets discarded 
+intersect_cols <- intersect(colnames(train_dtm), colnames(test_dtm))
+train_dtm <- as.data.table(train_dtm[, ..intersect_cols])
+test_dtm <- as.data.table(test_dtm[, ..intersect_cols])
+gc()
+C5 <- C5.0(fraudulent~., data=train_dtm, control = C5.0Control(minCases=mc))
+gc()
+#testing prediction performance on training data with model trained with training data
+cat("testing prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(C5, train_dtm)
+benefits_train_train_cm <- confusionMatrix(predictions, train_dtm$fraudulent, positive='1')
+print(benefits_train_train_cm)
+cat("\n\n")
+gc()
+cat("testing prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(C5, test_dtm)
+benefits_train_test_cm <- confusionMatrix(predictions, test_dtm$fraudulent, positive='1')
+print(benefits_train_test_cm)
+cat("\n\n\n")
+gc()
+
+
+#performing C5 on company_profile attribute
+cat("performing C5 on company_profile attribute\n\n")
+train_dtm <- fread("./text_data/company_profiles_train_DTM.csv")
+test_dtm <- fread("./text_data/company_profiles_test_DTM.csv")
+mc <- table(test_dtm$fraudulent)[2]
+train_dtm$fraudulent %<>% factor
+test_dtm$fraudulent %<>% factor
+#finding the column name intersection between training and testing partitions
+#so the terms match, (partitions are still different and valid), just making sure
+#the fields match and anything extra gets discarded 
+intersect_cols <- intersect(colnames(train_dtm), colnames(test_dtm))
+train_dtm <- as.data.table(train_dtm[, ..intersect_cols])
+test_dtm <- as.data.table(test_dtm[, ..intersect_cols])
+gc()
+C5 <- C5.0(fraudulent~., data=train_dtm, control = C5.0Control(minCases=mc))
+gc()
+#testing prediction performance on training data with model trained with training data
+cat("testing prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(C5, train_dtm)
+company_profiles_train_train_cm <- confusionMatrix(predictions, train_dtm$fraudulent, positive='1')
+print(company_profiles_train_train_cm)
+cat("\n\n")
+gc()
+cat("testing prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(C5, test_dtm)
+company_profiles_train_test_cm <- confusionMatrix(predictions, test_dtm$fraudulent, positive='1')
+print(company_profiles_train_test_cm)
+cat("\n\n\n")
+gc()
--- project1/text_svm.R
+++ project1/text_svm.R
@@ -18,84 +18,141 @@ library(dplyr)
 library(caret)  #for pre-processing, and model tuning
 library(e1071) #for svm 
 library(kernlab) #for svm
-
-#removes whitespace, punctuation, stopwords,
-# amongst other content that may negatively
-#affect model performance
-clean_corpus <- function(corpus){
-  corpus %<>% tm_map(., removeNumbers)
-  corpus %<>% tm_map(., stripWhitespace)
-  corpus %<>% tm_map(., removePunctuation)
-  corpus %<>% tm_map(., content_transformer(tolower))
-  corpus %<>% tm_map(., removeWords, stopwords("en"))
-  corpus %<>% tm_map(., content_transformer(stemDocument), language = "english")
-  
-  return(corpus)
-}
-
-#assuming train and test csv's exist
-#and that they are properly balanced 
-#and valid partitions
-jp_train <- read.csv('./other_data/jp_train.csv')
-jp_test <- read.csv('./other_data/jp_test.csv')
-
-
-#columns eligible for text mining are benefits, description, requirements and company_profiles
+library(data.table)
 
 
 
-jp.feature_corpus <- jp_train$benefits %>%
-                      as.character %>% 
-                      stri_replace_all_regex(str=., pattern = "[^A-Za-z\\s]+", replacement = "")
 
-jp.feature_corpus %<>% VectorSource 
-jp.feature_corpus %<>% VCorpus 
-jp.feature_corpus %<>% clean_corpus
+cat("performing KSVM on description attribute\n\n")
+train_dtm <- fread("./text_data/description_train_DTM.csv")
+test_dtm <- fread("./text_data/description_test_DTM.csv")
+#finding the column name intersection between training and testing partitions
+#so the terms match, (partitions are still different and valid), just making sure
+#the fields match and anything extra gets discarded 
+intersect_cols <- intersect(colnames(train_dtm), colnames(test_dtm))
+train_dtm <- as.data.table(train_dtm[, ..intersect_cols])
+test_dtm <- as.data.table(test_dtm[, ..intersect_cols])
 gc()
-#create a document term matrix for the benefits attribute
-dtm_train <- jp.feature_corpus %>% DocumentTermMatrix(., control=list(wordLengths=c(1, Inf))) %>% 
-                         as.matrix %>% as.data.frame
-
-dtm_train$fraudulent <- jp_train$fraudulent %>% factor
-dtm_train %<>% as.data.table()
-
-#training ksvm model on training data
-trained_ksvm <- ksvm(fraudulent~., data=dtm_train, kernel="rbfdot", sigma=.2)
+#KSVM hyperparameter sigma is tuned at .2 to avoid overfitting
+trained_ksvm <- ksvm(fraudulent~., data=train_dtm, kernel="rbfdot", sigma=.2)
 gc()
-#predicting training data from trained model
-predictions <- predict(trained_ksvm, dtm_train)
+#testing prediction performance on training data with model trained with training data
+cat("testing the model's prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(trained_ksvm, train_dtm)
 gc()
 #mapping to target variable class values with .5 as the probability threshold
-predictions <- ifelse(test=((1-predictions) >= .5), yes=0, no=1)
-confusion_matrix <- confusionMatrix(factor(predictions), factor(jp_train$fraudulent), positive = '1')
-print(confusion_matrix)
-
-
-
-jp.feature_corpus <- jp_test$benefits %>%
-                      as.character %>% 
-                      stri_replace_all_regex(str=., pattern = "[^A-Za-z\\s]+", replacement = "")
-
-jp.feature_corpus %<>% VectorSource 
-jp.feature_corpus %<>% VCorpus 
-jp.feature_corpus %<>% clean_corpus
-gc()
-#create a document term matrix for the benefits attribute
-dtm_test <- jp.feature_corpus %>% DocumentTermMatrix(., control=list(wordLengths=c(1, Inf))) %>% 
-                         as.matrix %>% as.data.frame
-
-dtm_test$fraudulent <- jp_test$fraudulent %>% factor
-dtm_test %<>% as.data.table()
-#testing trained model predictions on test data
-predictions <- predict(trained_ksvm, dtm_test)
-gc()
-
 predictions <- ifelse(test=((1-predictions) >= .5), yes=0, no=1 )
-predictions <- as.data.frame(predictions);
-rownames(predictions) <- rownames(predictions)
-confusion_matrix <- confusionMatrix(factor(predictions), factor(jp_test$fraudulent), positive = '1')
-print(confusion_matrix)
-
+description_train_train_cm <- confusionMatrix(factor(predictions), factor(train_dtm$fraudulent), positive = '1')
+print(description_train_train_cm)
+cat("\n\n")
+#testing prediction performance on test data with model trained with training data
+cat("testing the model's prediction performance on test data with model trained with training data\n\n")
+predictions <- predict(trained_ksvm, test_dtm)
 gc()
+#mapping to target variable class values with .5 as the probability threshold
+predictions <- ifelse(test=((1-predictions) >= .5), yes=0, no=1 )
+description_train_test_cm <- confusionMatrix(factor(predictions), factor(train_dtm$fraudulent), positive = '1')
+print(description_train_test_cm)
+cat("\n\n\n")
 
+
+
+cat("performing KSVM on requirements attribute\n\n")
+train_dtm <- fread("./text_data/requirements_train_DTM.csv")
+test_dtm <- fread("./text_data/requirements_test_DTM.csv")
+#finding the column name intersection between training and testing partitions
+#so the terms match, (partitions are still different and valid), just making sure
+#the fields match and anything extra gets discarded 
+intersect_cols <- intersect(colnames(train_dtm), colnames(test_dtm))
+train_dtm <- as.data.table(train_dtm[, ..intersect_cols])
+test_dtm <- as.data.table(test_dtm[, ..intersect_cols])
+gc()
+#KSVM hyperparameter sigma is tuned at .2 to avoid overfitting
+trained_ksvm <- ksvm(fraudulent~., data=train_dtm, kernel="rbfdot", sigma=.2)
+gc()
+#testing prediction performance on training data with model trained with training data
+cat("testing the model's prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(trained_ksvm, train_dtm)
+gc()
+#mapping to target variable class values with .5 as the probability threshold
+predictions <- ifelse(test=((1-predictions) >= .5), yes=0, no=1 )
+requirements_train_train_cm <- confusionMatrix(factor(predictions), factor(train_dtm$fraudulent), positive = '1')
+print(requirements_train_train_cm)
+cat("\n\n")
+#testing prediction performance on test data with model trained with training data
+cat("testing the model's prediction performance on test data with model trained with training data\n\n")
+predictions <- predict(trained_ksvm, test_dtm)
+gc()
+#mapping to target variable class values with .5 as the probability threshold
+predictions <- ifelse(test=((1-predictions) >= .5), yes=0, no=1 )
+requirements_train_test_cm <- confusionMatrix(factor(predictions), factor(test_dtm$fraudulent), positive = '1')
+print(requirements_train_test_cm)
+cat("\n\n\n")
+
+
+
+cat("performing KSVM on benefits attribute\n\n")
+train_dtm <- fread("./text_data/benefits_train_DTM.csv")
+test_dtm <- fread("./text_data/benefits_test_DTM.csv")
+#finding the column name intersection between training and testing partitions
+#so the terms match, (partitions are still different and valid), just making sure
+#the fields match and anything extra gets discarded 
+intersect_cols <- intersect(colnames(train_dtm), colnames(test_dtm))
+train_dtm <- as.data.table(train_dtm[, ..intersect_cols])
+test_dtm <- as.data.table(test_dtm[, ..intersect_cols])
+gc()
+#KSVM hyperparameter sigma is tuned at .2 to avoid overfitting
+trained_ksvm <- ksvm(fraudulent~., data=train_dtm, kernel="rbfdot", sigma=.2)
+gc()
+#testing prediction performance on training data with model trained with training data
+cat("testing the model's prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(trained_ksvm, train_dtm)
+gc()
+#mapping to target variable class values with .5 as the probability threshold
+predictions <- ifelse(test=((1-predictions) >= .5), yes=0, no=1 )
+benefits_train_train_cm <- confusionMatrix(factor(predictions), factor(train_dtm$fraudulent), positive = '1')
+print(benefits_train_train_cm)
+cat("\n\n")
+#testing prediction performance on test data with model trained with training data
+cat("testing the model's prediction performance on test data with model trained with training data\n\n")
+predictions <- predict(trained_ksvm, test_dtm)
+gc()
+#mapping to target variable class values with .5 as the probability threshold
+predictions <- ifelse(test=((1-predictions) >= .5), yes=0, no=1 )
+benefits_train_test_cm <- confusionMatrix(factor(predictions), factor(test_dtm$fraudulent), positive = '1')
+print(benefits_train_test_cm)
+cat("\n\n\n")
+
+
+cat("performing KSVM on company_profile attribute\n\n")
+train_dtm <- fread("./text_data/company_profiles_train_DTM.csv")
+test_dtm <- fread("./text_data/company_profiles_test_DTM.csv")
+#finding the column name intersection between training and testing partitions
+#so the terms match, (partitions are still different and valid), just making sure
+#the fields match and anything extra gets discarded 
+intersect_cols <- intersect(colnames(train_dtm), colnames(test_dtm))
+train_dtm <- as.data.table(train_dtm[, ..intersect_cols])
+test_dtm <- as.data.table(test_dtm[, ..intersect_cols])
+gc()
+#KSVM hyperparameter sigma is tuned at .2 to avoid overfitting
+trained_ksvm <- ksvm(fraudulent~., data=train_dtm, kernel="rbfdot", sigma=.2)
+gc()
+#testing prediction performance on training data with model trained with training data
+cat("testing the model's prediction performance on training data with model trained with training data\n\n")
+predictions <- predict(trained_ksvm, train_dtm)
+gc()
+#mapping to target variable class values with .5 as the probability threshold
+predictions <- ifelse(test=((1-predictions) >= .5), yes=0, no=1 )
+company_profiles_train_train_cm <- confusionMatrix(factor(predictions), factor(train_dtm$fraudulent), positive = '1')
+print(company_profiles_train_train_cm)
+cat("\n\n")
+#testing prediction performance on test data with model trained with training data
+cat("testing the model's prediction performance on test data with model trained with training data\n\n")
+predictions <- predict(trained_ksvm, test_dtm)
+gc()
+#mapping to target variable class values with .5 as the probability threshold
+predictions <- ifelse(test=((1-predictions) >= .5), yes=0, no=1 )
+company_profiles_train_test_cm <- confusionMatrix(factor(predictions), factor(test_dtm$fraudulent), positive = '1')
+print(company_profiles_train_test_cm)
+cat("\n\n\n")
 
